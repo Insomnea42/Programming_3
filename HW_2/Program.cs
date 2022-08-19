452bec90b98ef622e75cb651584a09e855b12d6d
insomnea42 <khveroe@gmail.com>
2022-08-19 12:39:06 +0500

    Выполнение дз к семинару №3

---
+++ HW_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите первую координату: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату: ");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double ab = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+Console.WriteLine(Math.Round(ab, 2));
---
+++ HW_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+bool ProvercaZnaka(int num)
+{
+    if (num > 0) return true;
+    else return false;
+}
+
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+bool pos = ProvercaZnaka(n);
+int i = 1;
+Console.Write("Таблица кубов до " + n + ": ");
+n = Math.Abs(n);
+if (n == 0)
+{
+    Console.Write(0);
+}
+else
+{
+    while (i <= n)
+    {
+        if (pos == true) Console.Write(Math.Pow(i, 3));
+        else Console.Write("-" + Math.Pow(i, 3));
+        i++;
+        if (i <= n) Console.Write(", ");
+    }
+}
+Console.WriteLine(";");
